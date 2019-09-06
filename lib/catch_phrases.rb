486c04783fb38558023c358cee559d53315d6ccd
Erica <ericabasak@gmail.com>
2019-09-06 17:04:45 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -6,3 +6,13 @@ end
 def toadstool
   puts "Thank You Mario! But Our Princess Is In Another Castle!"
 end
+
+def link 
+  puts "It's Dangerous To Go Alone! Take This."
+end
+
+def any_phrase(phrase)
+  puts "Do A Barrel Roll!"
+end
+
+  
